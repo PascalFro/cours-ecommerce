d1c32b642d4c5a0ee0dabac4a7ee5a8df1074d66
Pascal Fromentin <pndfromentin@gmail.com>
2022-03-07 15:01:17 +0100

    Mise en place de Doctrine et de nos premières entités.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function homepage(ProductRepository $productRepository): Response
     {
+        $products = $productRepository->findBy([], [], 3);
+        // dd($products);
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $products
         ]);
     }
 }
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Faker\Factory;
+use App\Entity\Product;
+use Liior\Faker\Prices;
+use Bezhanov\Faker\Provider\Commerce;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class AppFixtures extends Fixture
+{
+
+    protected $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+
+        for ($c = 0; $c < 3; $c++) {
+            $category = new Category();
+            $category->setName($faker->department())
+                ->setSlug(strtolower($this->slugger->slug($category->getName())));
+
+            $manager->persist($category);
+
+            for ($p = 0; $p < mt_rand(15, 20); $p++) {
+                $product = new Product;
+                $product->setName($faker->productName())
+                    ->setPrice($faker->price(4000, 20000))
+                    ->setSlug(strtolower($this->slugger->slug($product->getName())))
+                    ->setCategory($category)
+                    ->setShortDescription($faker->paragraph())
+                    ->setMainPicture($faker->imageUrl(400, 400, 'products', true));
+
+                $manager->persist($product);
+            }
+        }
+
+        $manager->flush();
+    }
+}
