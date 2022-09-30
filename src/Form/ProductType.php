248d3d3c4ab3eb911d1aa81a6259725289f0e71d
Pascal Fromentin <pndfromentin@gmail.com>
2022-09-30 17:10:13 +0200

    Découverte de fonctionnalités avancées du composant Form

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Product;
 use App\Entity\Category;
+use App\Form\DataTransformer\CentimesTransformer;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -27,7 +30,8 @@ class ProductType extends AbstractType
             ])
             ->add('price', MoneyType::class, [
                 'label' => 'Prix du produit',
-                'attr' => ['class' => 'form-control', 'placeholder' => 'Taper le prix du produit en €']
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Taper le prix du produit en €'],
+                'divisor' => 100
             ])
             ->add('mainPicture', UrlType::class, [
                 'label' => 'Image du produit',
@@ -43,6 +47,30 @@ class ProductType extends AbstractType
                     return strtoupper($category->getName());
                 }
             ]);
+
+        //$builder->get('price')->addModelTransformer(new CentimesTransformer);
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+
+            $form = $event->getForm();
+
+            /** @var Product */
+            $product = $event->getData();
+
+            /* Si, par exemple, nous voulions ne pas modifer la catégorie d'un produit lors d'une modification : */
+            if ($product->getId() === null) {
+                $form->add('category', EntityType::class, [
+                    'label' => 'Catégorie',
+                    'placeholder' => '-- Choisir une catégorie --',
+                    'class' => Category::class,
+                    // 'choice_label' => 'name'
+                    // ou alorss on passe une fonction qui s'effectue sur chaque entité (mise en majuscule de chaque catégorie dans notre exemple)
+                    'choice_label' => function (Category $category) {
+                        return strtoupper($category->getName());
+                    }
+                ]);
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
