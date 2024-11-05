d3bd627610714e939ace55e56ecf332c87c70eb2
Pascal Fromentin <pndfromentin@gmail.com>
2024-11-05 07:36:56 +0100

    Mise en place avec Docker

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,7 @@ use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Controller\ProductRepository;
+
 
 
 class HomeController extends AbstractController
@@ -16,19 +16,10 @@ class HomeController extends AbstractController
      */
     public function homepage(ProductRepository $productRepository): Response
     {
-<<<<<<< HEAD
-
-        $products = $productRepository->findBy([], [], 3);
-        dd($products);
-
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-=======
         $products = $productRepository->findBy([], [], 3);
         // dd($products);
         return $this->render('home/home.html.twig', [
             'products' => $products
->>>>>>> a35296f5f2d4861d8aba8c16cde95a0de0a233f3
         ]);
     }
 }
